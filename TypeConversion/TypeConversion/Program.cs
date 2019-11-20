5086ff00376d8d8cc851f4ff1794bc01679e923f
Martín Chiri <martinchiricc07@gmail.com>
2019-11-20 15:47:36 -0500

    TypeConversion v5.0 string to boolean

--- TypeConversion/TypeConversion/Program.cs
+++ TypeConversion/TypeConversion/Program.cs
@@ -8,13 +8,13 @@ namespace TypeConversion
         {
             try
             {
-                var number = "1234";
-                byte b = Convert.ToByte(number);
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
                 Console.WriteLine(b);
             }
             catch (Exception e)
             {
-                Console.WriteLine("The number could not be converted to a byte.");
+                Console.WriteLine("The string could not be converted to boolean.");
              
             }
             
