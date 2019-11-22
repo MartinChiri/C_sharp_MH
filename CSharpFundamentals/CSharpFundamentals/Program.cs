685ed7368e5f34fc17dbb1d7c6d406c77007fa01
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 17:48:09 -0500

    Arrays and Lists v1.6 List Add AddRange

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpFundamentals
 {
@@ -6,45 +7,13 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var numbers = new[] {3, 7, 9, 2, 14, 6};
-            
-            //Length
-            Console.WriteLine("Length: "+numbers.Length);
-
-            //IndexOf
-            var index = Array.IndexOf(numbers, 9);
-            Console.WriteLine("Index of 9: "+index);
-
-            //Clear
-            Array.Clear(numbers,0,2);
-
-            Console.WriteLine("Effect of Clear()");
-            foreach (var n in numbers)
-                Console.WriteLine(n);
-            
-
-            //Copy
-            var another=new int[3];
-            Array.Copy(numbers,another,3);
-
-            Console.WriteLine("Effect of Copy()");
-            foreach (var n in another)
-                Console.WriteLine(n);
-
-
-            //Sort
-            Array.Sort(numbers);
-
-            Console.WriteLine("Effect of Sort()");
-            foreach (var n in numbers)
-                Console.WriteLine(n);
-
-            //Reverse
-            Array.Reverse(numbers);
-
-            Console.WriteLine("Effect of Reverse()");
-            foreach (var n in numbers)
-                Console.WriteLine(n);
+            var numbers = new List<int>(){1,2,3,4};
+            numbers.Add(1);
+            numbers.AddRange(new int[3]{5,6,7});
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
         }
 
     }
