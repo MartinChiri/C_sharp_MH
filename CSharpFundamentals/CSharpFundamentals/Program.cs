ec76c90190d13c52d2e790415349f64b18f59012
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 15:39:38 -0500

    Control Flow v1.5 loop for decrement

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -11,6 +11,12 @@ namespace CSharpFundamentals
                 if(i%2==0)
                     Console.WriteLine(i);
             }
+
+            for (int i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
         }
 
     }
