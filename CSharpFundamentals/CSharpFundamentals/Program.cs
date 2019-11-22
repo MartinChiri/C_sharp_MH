49d8da567f59b0db4c8d17e6759b53c6d72fe008
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 17:52:18 -0500

    Arrays and Lists v1.8 List LastIndexOf

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -12,8 +12,11 @@ namespace CSharpFundamentals
             numbers.AddRange(new int[3]{5,6,7});
             foreach (var number in numbers)
                 Console.WriteLine(number);
-
+            
+            Console.WriteLine();
             Console.WriteLine("Index of 1: "+ numbers.IndexOf(1));
+            Console.WriteLine("Last index of 1: "+ numbers.LastIndexOf(1));
+            
         }
 
     }
