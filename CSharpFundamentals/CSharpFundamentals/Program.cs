febf99ff748e2e7ce7e0c99e7985e36d28cd3b9a
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 15:30:04 -0500

    Control Flow v1.2 switch enum

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -6,17 +6,20 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            bool isGoldCustomer = true;
+            var season = Season.Autumn;
 
-            //float price;
-            //if (isGoldCustomer)
-            //    price = 19.95f;
-            //else
-            //    price = 29.95f;
-
-            float price = (isGoldCustomer) ? 19.95f : 29.95f;
-
-            Console.WriteLine(price);
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's Autumn and a beautiful season.");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("It's perfect to go to beach.");
+                    break;
+                default:
+                    Console.WriteLine("I don't understand that season!");
+                    break;
+            }
         }
 
     }
