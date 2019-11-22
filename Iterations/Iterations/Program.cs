44969f3aca98666ad30919f2ef99c87eb55dbe30
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 16:12:53 -0500

    Control Flow v2.0 while infinite loop

--- Iterations/Iterations/Program.cs
+++ Iterations/Iterations/Program.cs
@@ -6,18 +6,15 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            //for (var i = 0; i < 10; i++)
-            //{
-            //    if(i%2==0)
-            //        Console.WriteLine(i);
-            //}
-
-            var i = 0;
-            while (i <= 10)
+            while (true)
             {
-                if(i%2==0)
-                    Console.WriteLine(i);
-                i++;
+                Console.Write("Type your name: ");
+                var input=Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+                
+                Console.WriteLine("@Echo: "+input);
             }
         }
     }
