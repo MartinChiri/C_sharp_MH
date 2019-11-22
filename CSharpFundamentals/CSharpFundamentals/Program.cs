ba152167bee4dc4a8765a6fa820fb81dba9a9b9d
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 18:12:46 -0500

    Working with Dates v1.0 DateTime Hour Minute

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace CSharpFundamentals
 {
@@ -7,30 +6,12 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int>(){1,2,3,4};
-            numbers.Add(1);
-            numbers.AddRange(new int[3]{5,6,7});
-            foreach (var number in numbers)
-                Console.WriteLine(number);
-            
-            Console.WriteLine();
-            Console.WriteLine("Index of 1: "+ numbers.IndexOf(1));
-            Console.WriteLine("Last index of 1: "+ numbers.LastIndexOf(1));
+            var dateTime=new DateTime(2015,1,1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
 
-            Console.WriteLine("Count : "+numbers.Count);
-
-            for (var i = 0; i < numbers.Count; i++)
-            {
-                if (numbers[i] == 1)
-                    numbers.Remove(numbers[i]);
-            }
-
-            foreach (var number in numbers)
-                Console.WriteLine(number);
-            
-            numbers.Clear();
-            Console.WriteLine("Count : " + numbers.Count);
-
+            Console.WriteLine("Hour: "+now.Hour);
+            Console.WriteLine("Minute: "+now.Minute);
 
         }
 
