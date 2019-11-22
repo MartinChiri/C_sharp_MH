3611b363c40cf6dd6866d062fc950486c9e8de50
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 15:20:22 -0500

    Control Flow v1.0 If else if else

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -2,31 +2,20 @@
 
 namespace CSharpFundamentals
 {
-    public class Person
-    {
-        public int Age;
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var number = 1;
-            Increment(number);
-            Console.WriteLine(number);
-
-            var person=new Person(){Age = 20};
-            MakeOld(person);
-            Console.WriteLine(person.Age);
+            int hour = 10;
+            
+            if(hour>0 && hour<12)
+                Console.WriteLine("It's morning.");
+            else if(hour>=12 && hour<18)
+                Console.WriteLine("It's afternoon.");
+            else
+                Console.WriteLine("It's evening.");
+            
         }
 
-        public static void Increment(int number)
-        {
-            number += 10;
-        }
-
-        public static void MakeOld(Person person)
-        {
-            person.Age += 10;
-        }
     }
 }
