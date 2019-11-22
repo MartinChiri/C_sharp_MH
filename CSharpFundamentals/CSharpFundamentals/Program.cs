ac5bdef6e1a358258c2175d91ad6e9b99a2d4f60
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 13:51:24 -0500

    array v3.0 strings

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -17,6 +17,10 @@ namespace CSharpFundamentals
             Console.WriteLine(flags[0]);
             Console.WriteLine(flags[1]);
             Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+
+
         }
     }
 }
