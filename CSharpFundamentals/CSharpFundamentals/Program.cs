dd2537c637a476e69241de7e02b5f182fb238381
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 16:06:20 -0500

    Control flow v1.9 while

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -6,12 +6,7 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var numbers = new int[] {1, 2, 3, 4};
 
-            foreach (var number in numbers)
-            {
-                Console.WriteLine(number);
-            }
         }
 
     }
