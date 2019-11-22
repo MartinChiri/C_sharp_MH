7190074cfbfaba5e694ddaa76becb169e37f74c4
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 15:57:15 -0500

    Control Flow v1.6 loop in a string

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -6,16 +6,10 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <=10; i++)
+            var name = "John Smith";
+            for (var i = 0; i < name.Length; i++)
             {
-                if(i%2==0)
-                    Console.WriteLine(i);
-            }
-
-            for (int i = 10; i >= 1; i--)
-            {
-                if (i % 2 == 0)
-                    Console.WriteLine(i);
+                Console.WriteLine(name[i]);
             }
         }
 
