5d520201b7187d5a1b22c0aa56ffea604bceffa2
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 13:59:40 -0500

    string v1.0 format join

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -6,21 +6,16 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var numbers = new int[3];
-            numbers[0] = 1;
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
+            var firstName = "Mosh";
+            var lastName = "Hamedani";
 
-            var flags = new bool[3];
-            flags[0] = true;
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
+            var fullName = firstName + " " + lastName;
 
-            var names = new string[3] { "Jack", "John", "Mary" };
+            var myFullName = string.Format("My names is {0} {1}", firstName, lastName);
 
-
+            var names=new string[3]{"John","Jack","Mary"};
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
         }
     }
 }
