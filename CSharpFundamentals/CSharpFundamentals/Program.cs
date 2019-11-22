063442b315da95853bdfe246761106cba60a3c47
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 15:06:31 -0500

    References Types and Value Types v2.0 arrays reference

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -11,6 +11,10 @@ namespace CSharpFundamentals
             b++;
             Console.WriteLine("a : {0},b : {1}",a,b);
 
+            var array1 = new int[3] {1, 2, 3};
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine("array1[0]: {0}, array2[0]: {1}",array1[0],array2[0]);
         }
     }
 }
