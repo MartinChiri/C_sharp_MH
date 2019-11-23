356bba184c0fc1716b31565eee084e19ead41f0f
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 20:44:45 -0500

    Working with Text v1.24 Procedural Programming Initial Second Program

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace CSharpFundamentals
@@ -7,21 +8,34 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What's your name?");
-            var name = Console.ReadLine();
-            var reversed = ReverseName(name);
-            Console.WriteLine("Reversed name: "+reversed);
+
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number (or 'Quit' to exit):");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                    break;
+                numbers.Add(Convert.ToInt32(input));
+
+            }
+
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if(!uniques.Contains(number))
+                    uniques.Add(number);
+            }
+
+            Console.WriteLine("Unique numbers: ");
+            foreach (var number in uniques)
+            {
+                Console.WriteLine(number);
+            }
         }
 
-        public static string ReverseName(string name)
-        {
-            var array = new char[name.Length];
-            for (var i = name.Length; i > 0; i--)
-                array[name.Length - i] = name[i - 1];
-
-            return new string(array);
-
-        }
 
 
 
