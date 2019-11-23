804170d3e40ddd84bfffa5e196177e5e89334636
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 20:38:57 -0500

    Working with Text v1.23 Procedural Programming Final Program

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -9,17 +9,21 @@ namespace CSharpFundamentals
         {
             Console.WriteLine("What's your name?");
             var name = Console.ReadLine();
-
-            var array = new char[name.Length];
-            for (var i = name.Length; i >0 ; i--)
-            {
-                array[name.Length - i] = name[i - 1];
-            }
-            var reversed = new string(array);
+            var reversed = ReverseName(name);
             Console.WriteLine("Reversed name: "+reversed);
         }
 
-        
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+            return new string(array);
+
+        }
+
+
 
     }
 }
