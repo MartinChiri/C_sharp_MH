696709a402a951c102d449547819ed2bdccdf218
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 20:21:37 -0500

    Working with Text v1.20 StringBuilder accessing by index

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -21,7 +21,8 @@ namespace CSharpFundamentals
             builder.Insert(0, new string('-', 10));
 
             Console.WriteLine(builder);
-            
+
+            Console.WriteLine("First char: "+builder[0]);
         }
 
         
