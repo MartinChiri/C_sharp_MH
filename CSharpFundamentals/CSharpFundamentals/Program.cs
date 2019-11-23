5dc1e420540885ad9a8e0b5e752d47d2c2602820
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 22:48:22 -0500

    Debugging Tools in Visual Studio v1.2 1st Program side effects removed

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -8,7 +8,7 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int> {1, 2, 3, 4, 5, 6};
+            var numbers = new List<int> {1, 2};
             var smallests = GetSmallests(numbers, 3);
 
             foreach (var number in smallests)
@@ -18,12 +18,13 @@ namespace CSharpFundamentals
         }
         public static List<int> GetSmallests(List<int> list, int count)
         {
+            var buffer=new List<int>(list);
             var smallests=new List<int>();
             while (smallests.Count<count)
             {
-                var min = GetSmallest(list);
+                var min = GetSmallest(buffer);
                 smallests.Add(min);
-                list.Remove(min);
+                buffer.Remove(min);
             }
             return smallests;
         }
