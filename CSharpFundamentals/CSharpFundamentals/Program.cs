5772174155ec53630fa18a19aa2495c17e64bf21
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 22:16:40 -0500

    Debugging Tools in Visual Studio v1.0 1st Program

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace CSharpFundamentals
 {
@@ -7,17 +8,40 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var path = @"C:\Projects\CSharpFundamentals\HelloWorld\HelloWorld.sln";
+            var numbers = new List<int> {1, 2, 3, 4, 5, 6};
+            var smallests = GetSmallests(numbers, 3);
 
-            var dotIndex = path.IndexOf('.');
-            var extension = path.Substring(dotIndex);
-
-            Console.WriteLine("Extension: "+Path.GetExtension(path));
-            Console.WriteLine("File Name: "+Path.GetFileName(path));
-            Console.WriteLine("File Name without Extension: "+Path.GetFileNameWithoutExtension(path));
-            Console.WriteLine("Directory Name: "+Path.GetDirectoryName(path));
-            
+            foreach (var number in smallests)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            var smallests=new List<int>();
+            while (smallests.Count<count)
+            {
+                var min = GetSmallest(list);
+                smallests.Add(min);
+                list.Remove(min);
+            }
+            return smallests;
         }
 
+        public static int GetSmallest(List<int> list)
+        {
+            //Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (list[i] > min)
+                    min = list[i];
+            }
+            return min;
+        }
+
+
     }
+
 }
+      
