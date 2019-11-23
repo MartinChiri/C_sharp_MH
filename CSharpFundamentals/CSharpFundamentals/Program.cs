1a07d891684cb364b37d36099ab80619172c647e
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 20:12:29 -0500

    Working with Text v1.15 StringBuilder AppendLine

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -9,7 +9,12 @@ namespace CSharpFundamentals
         {
             var builder=new StringBuilder();
             builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
             Console.WriteLine(builder);
+            
         }
 
         
