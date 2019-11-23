71a7b3787b250996abd3fc70c52ba3803cc75c8e
Martín Chiri <martinchiricc07@gmail.com>
2019-11-22 20:04:53 -0500

    Working with Text v1.13 public class StringUtility

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 
 namespace CSharpFundamentals
 {
@@ -10,32 +7,11 @@ namespace CSharpFundamentals
         static void Main(string[] args)
         {
             var sentence = "This is going to be a really really really really really really long text.";
-            var summary = SummarizeText(sentence);
+            var summary = StringUtility.SummarizeText(sentence,25);
             Console.WriteLine(summary);
 
         }
 
-        static string SummarizeText(string text,int maxLength=20)
-        {
-
-            if (text.Length < maxLength)
-                return text;
-
-            var words = text.Split(" ");
-            var totalCharacters = 0;
-            var summaryWords = new List<string>();
-
-            foreach (var word in words)
-            {
-                summaryWords.Add(word);
-                totalCharacters += word.Length + 1;
-                if (totalCharacters > maxLength)
-                    break;
-            }
-
-            
-            return String.Join(" ", summaryWords) + "...";
-        }
         
 
     }
